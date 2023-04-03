521654bf4f86a81f45621a5e84eb5f05f122042b
leandrovsk <contas.center@gmail.com>
2023-04-03 07:25:26 -0300

    feat: task 0 completed

---
+++ utils.py
@@ -0,0 +1,15 @@
+from exceptions import ImpossibleTitlesError, InvalidYearCupError, NegativeTitlesError
+from datetime import datetime
+
+
+def data_processing(data):
+    current_date = datetime.today()
+    first_cup_date = datetime.strptime(data["first_cup"], "%Y-%m-%d")
+    max_titles = (current_date.year - first_cup_date.year) / 4
+
+    if data["titles"] < 0:
+        raise NegativeTitlesError("titles cannot be negative")
+    if first_cup_date.year < 1930 or (first_cup_date.year - 1930) % 4 != 0:
+        raise InvalidYearCupError("there was no world cup this year")
+    if max_titles < data["titles"]:
+        raise ImpossibleTitlesError("impossible to have more titles than disputed cups")
